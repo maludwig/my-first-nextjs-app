59afe4e022b08c64ec6a9472846cca9d28a94f8c
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2023-06-23 12:02:00 +1200

    Added headers

--- src/app/dates/page.tsx
+++ src/app/dates/page.tsx
@@ -1,7 +1,25 @@
 import { DateTime } from "luxon";
+import { headers } from "next/headers";
 
 export default function Dates() {
+  const headersList = headers();
+  const referer = headersList.get("referer");
+
   const utcnow = DateTime.utc();
   const timestamp = utcnow.toFormat("yyyy-MM-dd HH:mm:ss");
-  return <p>Server Time: {timestamp}</p>;
+  const headerListElems = [];
+  for (const [key, value] of headersList.entries()) {
+    headerListElems.push(
+      <li>
+        {key}: {value}
+      </li>
+    );
+  }
+  return (
+    <div>
+      <p>Server Time: {timestamp}</p>
+      <h3>Headers:</h3>
+      <ul>{headerListElems}</ul>
+    </div>
+  );
 }
